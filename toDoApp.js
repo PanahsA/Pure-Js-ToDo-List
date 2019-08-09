049fcd1957034f38dfe7f49183ff5bbd45791cbb
PanahsA <ebri_bmg@hotmail.com>
2019-08-09 16:59:05 +0300

    seems good

--- toDoApp.js
+++ toDoApp.js
@@ -4,7 +4,7 @@ var toDoTd;
 var input = document.querySelector("input");
 var newT = document.querySelector(".new");
 var toDosCounter = 0;
-var trashCan = document.querySelector(".trashCan");
+// var trashCan = document.querySelector(".trashCan");
 var trashSpan = document.querySelector(".trashSpan");
 // var toDoTd = document.querySelector(".toDoTd");
 var pluss = document.querySelector("#pluss");
@@ -15,63 +15,142 @@ input.addEventListener("keyup", function (e) {
 
     if (e.keyCode === 13) {
 
-        //Fill the array with ToDOs obj with value and a serial number
-        toDosArray[enterHits] =
-            {
-                name: input.value,
-                number: enterHits
-            }
-        // Add new row with the new To-Do, the new trashcan btn and its span
-        newT.insertAdjacentHTML("beforebegin",
-            "<td class=\"toDoTd\" \"><button class=\"trashCan\">" +
-            " <i class=\"fas fa-lg fa-trash-alt\"></i></button><span class=\"trashSpan\"></span>"
-            + input.value + " </td> ");
+        // //Fill the array with ToDOs obj with value and a serial number
+        // toDosArray[enterHits] =
+        //     {
+        //         name: input.value,
+        //         number: enterHits
+        //     }
+        // // Add new row with the new To-Do, the new trashcan btn and its span
+        // newT.insertAdjacentHTML("beforebegin",
+        //     "<td class=\"toDoTd\" \"><button class=\"trashCan\">" +
+        //     " <i class=\"fas fa-lg fa-trash-alt\"></i></button><span class=\"trashSpan\"></span>"
+        //     + input.value + " </td> ");
+        //
+        // // Add strikethrough
+        // var toDoTd = document.querySelectorAll(".toDoTd");
+        // toDoTd[toDosCounter].addEventListener("click", function () {
+        //     this.classList.toggle("done");
+        // })
+        // //Console.log for debugging
+        // toDosArray.forEach(function (i) {
+        //     if( i.name!==undefined)
+        //     console.log("ta toDOs apo toDosArray: " + i.name + " at position : "+ i.number);
+        // })
+        // console.log("current length: "+toDosArray.length);
+        // console.log("\n")
+        //
+        // //Add id to current trashcan (use it for deletion)
+        // var trashCan = document.querySelectorAll(".trashCan");
+        // trashCan[enterHits].setAttribute("id", enterHits);
 
-        // Add strikethrough
-        var toDoTd = document.querySelectorAll(".toDoTd");
-        toDoTd[toDosCounter].addEventListener("click", function () {
-            this.classList.toggle("done");
-        })
-        //Console.log for debugging
-        toDosArray.forEach(function (i) {
-            if( i.name!==undefined)
-            console.log("ta toDOs apo toDosArray: " + i.name + " at position : "+ i.number);
-        })
-        console.log("current length: "+toDosArray.length);
-        console.log("\n")
+        createToDo();
 
-        //Add id to current trashcan (use it for deletion)
-        var trashCan = document.querySelectorAll(".trashCan");
-        trashCan[enterHits].setAttribute("id", enterHits);
-
-        // DELETE ITEM
-        trashCan[toDosCounter].addEventListener("click", function () {
-            // get the id of the trashcan to delete the the equivalent object.number
-            var itemToDelete = this.getAttribute("id");
-            console.log("item to delete:" + itemToDelete);
-            //if trashcan id matches serial number of item, delete it
-            toDosArray.forEach(function (obj) {
-                if (obj.number === Number(itemToDelete)) {
-                    console.log("about to kill obj with serial number: " + obj.number );
-                    toDosArray.splice(itemToDelete, 1, "");
-                }
-            })
-            toDoTd[itemToDelete].style.display = "none";
-            console.log("\n");
-        })
-
-
-        enterHits++;
-        toDosCounter++;
-        input.value = "";
+        // // DELETE ITEM
+        // trashCan[toDosCounter].addEventListener("click", function () {
+        //     // get the id of the trashcan to delete the the equivalent object.number
+        //     var itemToDelete = this.getAttribute("id");
+        //     console.log("item to delete:" + itemToDelete);
+        //     //if trashcan id matches serial number of item, delete it
+        //     toDosArray.forEach(function (obj) {
+        //         if (obj.number === Number(itemToDelete)) {
+        //             console.log("about to kill obj with serial number: " + obj.number);
+        //             toDosArray.splice(itemToDelete, 1, "");
+        //         }
+        //     });
+        //     toDoTd[itemToDelete].style.display = "none";
+        //     console.log("\n");
+        // });
+        // enterHits++;
+        // toDosCounter++;
+        // input.value = "";
     }
 });
 
 
+function isAllSpace(string){
+    var counter=0;
+    for (var i = 0; i < string.length+1; i++) {
+        if(string[i]==" ") counter++;
+    }
+    if(counter==string.length) return true;
+    else
+        return false;
+}
+
+function createToDo() {
+    console.log("from create");
+
+    //Fill the array with ToDOs obj with value and a serial number
+    toDosArray[enterHits] =
+        {
+            name: input.value,
+            number: enterHits
+        }
+
+    // Add new row with the new To-Do, the new trashcan btn and its span
+    // Dont add if the input is empty or space or spaces
+    if (input.value != "") {
+        if (!isAllSpace(input.value)) {
+            newT.insertAdjacentHTML("beforebegin",
+                "<td class=\"toDoTd\" \"><button class=\"trashCan\">" +
+                " <i class=\"fas fa-lg fa-trash-alt\"></i></button><span class=\"trashSpan\"></span>"
+                + input.value + " </td> ");
+
+        }
+    }
+
+
+    // Add strikethrough
+    var toDoTd = document.querySelectorAll(".toDoTd");
+    toDoTd[toDosCounter].addEventListener("click", function () {
+        this.classList.toggle("done");
+    })
+    //Console.log for debugging
+    toDosArray.forEach(function (i) {
+        if (i.name !== undefined)
+            console.log("ta toDOs apo toDosArray: " + i.name + " at position : " + i.number);
+    })
+    console.log("current length: " + toDosArray.length);
+    console.log("\n")
+
+    //Add id to current trashcan (use it for deletion)
+    var trashCan = document.querySelectorAll(".trashCan");
+    console.log("trashcan id: " + enterHits);
+    trashCan[enterHits].setAttribute("id", enterHits);
+
+    //DELETE
+    // Add event listener to trashcan for deletion
+    trashCan[toDosCounter].addEventListener("click", function (e) {
+        // get the id of the trashcan to delete the the equivalent object.number
+        e.stopPropagation();
+
+        var itemToDelete = this.getAttribute("id");
+        this.parentNode.parentNode.remove(itemToDelete.selectedIndex);
+
+
+
+        console.log("item to delete:" + itemToDelete);
+        //if trashcan id matches serial number of item, delete it
+        toDosArray.forEach(function (obj) {
+            if (obj.number === Number(itemToDelete)) {
+                console.log("about to kill obj with serial number: " + obj.number);
+                toDosArray.splice(itemToDelete, 1, "");
+            }
+        });
+        // createToDo();
+        // toDoTd[itemToDelete].style.display = "none";
+        console.log("\n");
+    });
+    enterHits++;
+    toDosCounter++;
+    input.value = "";
+
+}
+
 
 //Epilegei span kai to diwxnei mazi me ton kado
 var trashSpan = document.querySelectorAll(".trashSpan");
-
 
 
 pluss.addEventListener("click", function () {
